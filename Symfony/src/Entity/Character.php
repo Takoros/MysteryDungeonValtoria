e4f4448a2ecc84e9d083e7467c81f135ba6f9bf8
Steven Breziat <69626795+Takoros@users.noreply.github.com>
2023-01-03 23:49:05 +0100

    Ajoute l'entity Character
    Dans le but de mettre en place la base de donnée, l'entity Character
    est créé avec tout ses attributs. De plus, la table User est modifiée
    afin de faire le lien avec Character.
    
    http://193.168.145.76:8080/issues/2

---
+++ Symfony/src/Entity/Character.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $gender = null;
+
+    #[ORM\Column]
+    private ?int $age = null;
+
+    #[ORM\Column(length: 200, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $level = null;
+
+    #[ORM\Column]
+    private ?int $xp = null;
+
+    #[ORM\Column]
+    private ?int $statPoints = null;
+
+    #[ORM\Column]
+    private ?int $rank = null;
+
+    #[ORM\OneToOne(mappedBy: 'Character', cascade: ['persist', 'remove'])]
+    private ?User $userI = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getXp(): ?int
+    {
+        return $this->xp;
+    }
+
+    public function setXp(int $xp): self
+    {
+        $this->xp = $xp;
+
+        return $this;
+    }
+
+    public function getStatPoints(): ?int
+    {
+        return $this->statPoints;
+    }
+
+    public function setStatPoints(int $statPoints): self
+    {
+        $this->statPoints = $statPoints;
+
+        return $this;
+    }
+
+    public function getRank(): ?int
+    {
+        return $this->rank;
+    }
+
+    public function setRank(int $rank): self
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+
+    public function getUserI(): ?User
+    {
+        return $this->userI;
+    }
+
+    public function setUserI(?User $userI): self
+    {
+        // unset the owning side of the relation if necessary
+        if ($userI === null && $this->userI !== null) {
+            $this->userI->setCharacter(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($userI !== null && $userI->getCharacter() !== $this) {
+            $userI->setCharacter($this);
+        }
+
+        $this->userI = $userI;
+
+        return $this;
+    }
+}
