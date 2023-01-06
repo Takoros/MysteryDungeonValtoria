99b9c72766125f61c2d53394ce3296772a10ff40
Steven Breziat <69626795+Takoros@users.noreply.github.com>
2023-01-06 15:26:00 +0100

    Ajoute la commande CreateTypesSpeciesCommand
    Actuellement, nous avons aucun moyen de vérifier et de modifier la base
    de données selon les données de base nécessaire à l'application. Nous
    venons donc ajouter cette commande qui va venir comparer les fichiers
    .json et la BDD et effectue les modifications nécessaire.
    
    De plus, des modifications sur les entités Type et Species ont été
    nécessaire suite à une erreur de conception, changement de relation
    OneToMany à ManyToMany.
    
    https://project.takerra.fr/issues/3

--- Symfony/src/Entity/Type.php
+++ Symfony/src/Entity/Type.php
@@ -11,7 +11,6 @@ use Doctrine\ORM\Mapping as ORM;
 class Type
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
@@ -21,21 +20,28 @@ class Type
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $attackFile = null;
 
-    #[ORM\OneToMany(mappedBy: 'Type', targetEntity: Species::class)]
-    private Collection $Species;
-
     #[ORM\OneToMany(mappedBy: 'Type', targetEntity: Attack::class)]
     private Collection $Attacks;
 
+    #[ORM\ManyToMany(targetEntity: Species::class, mappedBy: 'Type')]
+    private Collection $Species;
+
     public function __construct()
     {
-        $this->Species = new ArrayCollection();
         $this->Attacks = new ArrayCollection();
+        $this->Species = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
     }
 
     public function getName(): ?string
@@ -58,36 +64,6 @@ class Type
     public function setAttackFile(?string $attackFile): self
     {
         $this->attackFile = $attackFile;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Species>
-     */
-    public function getSpecies(): Collection
-    {
-        return $this->Species;
-    }
-
-    public function addSpecies(Species $species): self
-    {
-        if (!$this->Species->contains($species)) {
-            $this->Species->add($species);
-            $species->setType($this);
-        }
-
-        return $this;
-    }
-
-    public function removeSpecies(Species $species): self
-    {
-        if ($this->Species->removeElement($species)) {
-            // set the owning side to null (unless already changed)
-            if ($species->getType() === $this) {
-                $species->setType(null);
-            }
-        }
 
         return $this;
     }
@@ -117,6 +93,33 @@ class Type
             if ($attack->getType() === $this) {
                 $attack->setType(null);
             }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Species>
+     */
+    public function getSpecies(): Collection
+    {
+        return $this->Species;
+    }
+
+    public function addSpecies(Species $species): self
+    {
+        if (!$this->Species->contains($species)) {
+            $this->Species->add($species);
+            $species->addType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSpecies(Species $species): self
+    {
+        if ($this->Species->removeElement($species)) {
+            $species->removeType($this);
         }
 
         return $this;
