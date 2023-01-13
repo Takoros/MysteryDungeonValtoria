1c0627ab782e543bf885a7640e1f8c49e65b06b9
Steven Breziat <69626795+Takoros@users.noreply.github.com>
2023-01-13 01:01:01 +0100

    Ajoute un SpeciesFormatter
    Dans le cadre de la création d'un controller permettant d'envoyer des
    données lorsqu'une route est appellée, par exemple, une route renvoyant
    les species existant, un formatter est nécessaire afin de renvoyer
    des données propres en JSON, les entitées Symfony ne pouvant être
    transmises directement.

---
+++ Symfony/src/Formatter/SpeciesFormatter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Formatter;
+
+class SpeciesFormatter
+{
+    public function listSpeciesFormat($speciesList){
+        $formattedSpeciesList = [];
+
+        foreach ($speciesList as $species) {
+            $typeList = [];
+
+            foreach ($species->getType() as $type){
+                $typeList[] = [
+                    "id" => $type->getId(),
+                    "name" => $type->getName()
+                ];
+            }
+
+            $formattedSpeciesList[] = [
+                "id" => $species->getId(),
+                "name" => $species->getName(),
+                "isPlayable" => $species->isIsPlayable(),
+                "types" => $typeList
+            ];
+        }
+
+        return $formattedSpeciesList;
+    }
+}
