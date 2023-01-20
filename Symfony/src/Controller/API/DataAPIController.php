117f2f13db43bc48d6ea247d986ef0e502b5444a
Steven Breziat <69626795+Takoros@users.noreply.github.com>
2023-01-20 23:11:27 +0100

    Modifie le comportement des controllers DataAPIController et MainAPIController
    Suite aux changements lié à l'ajout du abstractAPIController, la
    manière de ces deux controllers de gérer leur routes à été changée.

--- Symfony/src/Controller/API/DataAPIController.php
+++ Symfony/src/Controller/API/DataAPIController.php
@@ -5,12 +5,11 @@ namespace App\Controller\API;
 use App\Formatter\SpeciesFormatter;
 use App\Repository\SpeciesRepository;
 use App\Service\APIService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DataAPIController extends AbstractController
+class DataAPIController extends AbstractAPIController
 {
     private $apiService;
 
@@ -24,10 +23,7 @@ class DataAPIController extends AbstractController
     public function listSpecies(Request $request, SpeciesRepository $speciesRepository, SpeciesFormatter $speciesFormatter): JsonResponse
     {
         $post = json_decode($request->getContent());
-
-        if(empty($post->token) || !$this->apiService->isCorrectToken($post->token)){
-            return new JsonResponse(['message' => 'Unauthorized'], 401);
-        }
+        $isValid = $this->verifyTokenAndData($post, [], $this->apiService);
 
         $species = $speciesRepository->findAll();
         return new JsonResponse($speciesFormatter->listSpeciesFormat($species));
