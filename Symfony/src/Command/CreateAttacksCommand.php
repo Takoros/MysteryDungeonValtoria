bc2b7d35a339eb3d8cb6d139c09ac753174bad00
Steven Breziat <Takoros>
2023-06-21 16:42:24 +0200

    met a jour la commande de création d'attaque

--- Symfony/src/Command/CreateAttacksCommand.php
+++ Symfony/src/Command/CreateAttacksCommand.php
@@ -22,7 +22,25 @@ use Symfony\Component\HttpKernel\KernelInterface;
 )]
 class CreateAttacksCommand extends Command
 {
+    const BUG_ATTACKS_FILE = '/src/Data/attacks/bug_attacks.json';
+    const POISON_ATTACKS_FILE = '/src/Data/attacks/poison_attacks.json';
+    const GHOST_ATTACKS_FILE = '/src/Data/attacks/ghost_attacks.json';
+    const DARK_ATTACKS_FILE = '/src/Data/attacks/dark_attacks.json';
+    const FLYING_ATTACKS_FILE = '/src/Data/attacks/flying_attacks.json';
+    const STEEL_ATTACKS_FILE = '/src/Data/attacks/steel_attacks.json';
+    const GROUND_ATTACKS_FILE = '/src/Data/attacks/ground_attacks.json';
+    const ROCK_ATTACKS_FILE = '/src/Data/attacks/rock_attacks.json';
+    const WATER_ATTACKS_FILE = '/src/Data/attacks/water_attacks.json';
+    const FIRE_ATTACKS_FILE = '/src/Data/attacks/fire_attacks.json';
     const EXPLORER_ATTACKS_FILE = '/src/Data/attacks/explorer_attacks.json';
+    const GRASS_ATTACKS_FILE = '/src/Data/attacks/grass_attacks.json';
+    const ELECTRIC_ATTACKS_FILE = '/src/Data/attacks/electric_attacks.json';
+    const NORMAL_ATTACKS_FILE = '/src/Data/attacks/normal_attacks.json';
+    const ICE_ATTACKS_FILE = '/src/Data/attacks/ice_attacks.json';
+    const FIGHTING_ATTACKS_FILE = '/src/Data/attacks/fighting_attacks.json';
+    const PSY_ATTACKS_FILE = '/src/Data/attacks/psy_attacks.json';
+    const FAIRY_ATTACKS_FILE = '/src/Data/attacks/fairy_attacks.json';
+    const DRAGON_ATTACKS_FILE = '/src/Data/attacks/dragon_attacks.json';
 
     private $entityManager;
     private $attackRepository;
@@ -41,9 +59,46 @@ class CreateAttacksCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $bugAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::BUG_ATTACKS_FILE));
+        $poisonAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::POISON_ATTACKS_FILE));
+        $ghostAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::GHOST_ATTACKS_FILE));
+        $darkAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::DARK_ATTACKS_FILE));
+        $flyingAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::FLYING_ATTACKS_FILE));
+        $steelAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::STEEL_ATTACKS_FILE));
+        $groundAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::GROUND_ATTACKS_FILE));
+        $rockAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::ROCK_ATTACKS_FILE));
+        $waterAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::WATER_ATTACKS_FILE));
+        $fireAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::FIRE_ATTACKS_FILE));
         $explorerAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::EXPLORER_ATTACKS_FILE));
+        $grassAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::GRASS_ATTACKS_FILE));
+        $electricAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::ELECTRIC_ATTACKS_FILE));
+        $normalAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::NORMAL_ATTACKS_FILE));
+        $iceAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::ICE_ATTACKS_FILE));
+        $fightingAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::FIGHTING_ATTACKS_FILE));
+        $psyAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::PSY_ATTACKS_FILE));
+        $fairyAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::FAIRY_ATTACKS_FILE));
+        $dragonAttackListJson = json_decode(file_get_contents($this->kernel->getProjectDir() . self::DRAGON_ATTACKS_FILE));
 
-        $attackListJson = (object) array_merge((array) $explorerAttackListJson);
+        $attackListJson = (object) array_merge((array) $bugAttackListJson,
+                                               (array) $poisonAttackListJson,
+                                               (array) $ghostAttackListJson,
+                                               (array) $darkAttackListJson,
+                                               (array) $flyingAttackListJson,
+                                               (array) $steelAttackListJson,
+                                               (array) $groundAttackListJson,
+                                               (array) $rockAttackListJson,
+                                               (array) $waterAttackListJson,
+                                               (array) $fireAttackListJson,
+                                               (array) $explorerAttackListJson,
+                                               (array) $grassAttackListJson,
+                                               (array) $electricAttackListJson,
+                                               (array) $normalAttackListJson,
+                                               (array) $iceAttackListJson,
+                                               (array) $fightingAttackListJson,
+                                               (array) $psyAttackListJson,
+                                               (array) $fairyAttackListJson,
+                                               (array) $dragonAttackListJson);
+
         $attackListDatabase = $this->attackRepository->findAll();
 
         /**
