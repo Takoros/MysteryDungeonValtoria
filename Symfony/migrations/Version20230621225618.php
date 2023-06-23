ae2ff02811671ed9639c2edf69dadede21ec7675
Steven Breziat <Takoros>
2023-06-23 17:30:39 +0200

    Ajoute le système de log des combats
    Dans ce commit vous trouverez :
    
    - Une classe LogLine permettant de représenter au sein d'un CombatLog,
      une ligne de combat (Perte de pv, lancement d'attaque..)
    - Des modifications de CombatLog, ajoutant des fonctions afin de
      permettre la création de LogLine ainsi que l'enregistrement de ceux-ci
      en base et le chargement lors de la récupération depuis la base.
    - La migration permettant d'ajouter les LogLine au model CombatLog

---
+++ Symfony/migrations/Version20230621225618.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230621225618 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE combat_log ADD log_lines LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE combat_log DROP log_lines');
+    }
+}
