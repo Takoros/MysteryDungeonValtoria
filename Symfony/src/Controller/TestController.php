f61c45fb40e7680de1056f2aae374ab0462b8cb3
Steven Breziat <Takoros>
2023-06-23 17:37:06 +0200

    Modifie le Test Controller afin qu'il intègre la gestion des nouveaux logs

--- Symfony/src/Controller/TestController.php
+++ Symfony/src/Controller/TestController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CombatLog;
 use App\Repository\AttackRepository;
 use App\Repository\CharacterRepository;
+use App\Repository\CombatLogRepository;
 use App\Service\Combat\Arena;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function index(CharacterRepository $characterRepository, AttackRepository $attackRepository): Response
+    public function index(CharacterRepository $characterRepository, AttackRepository $attackRepository, EntityManagerInterface $em): Response
     {
         $char1 = $characterRepository->find(3); // Takoros
         $char2 = $characterRepository->find(4); // Lameterra
@@ -22,8 +26,22 @@ class TestController extends AbstractController
         $arena = new Arena([$char1, $char2],[$char3, $char4], Arena::TYPE_PVP, $attackRepository);
         $arena->launchBattle();
 
-        return $this->render('test.html.twig', [
-            'logs' => $arena->combatLog->getLogContent()
+        $arena->combatLog->saveCombatLog($em);
+
+        return $this->render('combatLog.html.twig', [
+            'displayableLogs' => $arena->combatLog->getDisplayableLogs(),
+            'combatLog' => $arena->combatLog
+        ]);
+    }
+
+    #[Route('/test2', name: 'app_test2')]
+    public function test2(CombatLogRepository $combatLogRepository): Response
+    {
+        $combatLog = $combatLogRepository->find(340);
+
+        return $this->render('combatLog.html.twig', [
+            'displayableLogs' => $combatLog->getDisplayableLogs(),
+            'combatLog' => $combatLog
         ]);
     }
 }
