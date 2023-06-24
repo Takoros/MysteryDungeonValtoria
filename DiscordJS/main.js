410e74b4e00ffb6e70a7607faea8dc5e6466666f
Steven Breziat <Takoros>
2023-06-24 18:29:43 +0200

    Ajoute la commande de création de personnage
    On modifie également le fonctionnement de récupération des choix liés
    aux espèces, en passant par un appel api.

--- DiscordJS/main.js
+++ DiscordJS/main.js
@@ -36,34 +36,52 @@ switch(process.env.STATUS) {
         console.log('Connecting with DEV/DEFAULT');
 }
 
-// Loading commands
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+(async () => {
+	try {
+        // Loading commands
+        client.commands = new Collection();
+        const commandsPath = path.join(__dirname, 'commands');
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath)
+
+            if(command.data instanceof Promise){
+                const commandData = await command.data;
+
+                if (commandData && 'execute' in command) {
+                    client.commands.set(commandData.name, command);
+                } else {
+                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+                }
+            }
+            else {
+                // Set a new item in the Collection with the key as the command name and the value as the exported module
+                if ('data' in command && 'execute' in command) {
+                    client.commands.set(command.data.name, command);
+                } else {
+                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+                }
+            }
+        }
+
+        // Loading events
+        const eventsPath = path.join(__dirname, 'events');
+        const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+        for (const file of eventFiles) {
+            const filePath = path.join(eventsPath, file);
+            const event = require(filePath);
+            if (event.once) {
+                client.once(event.name, (...args) => event.execute(...args));
+            } else {
+                client.on(event.name, (...args) => event.execute(...args));
+            }
+        }
+
+        client.login(client.env.get("token")); 
+	} catch (error) {
+        console.log(error);
 	}
-}
-
-// Loading events
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-client.login(client.env.get("token")); 
+})();
