ae1af3cd5a1a32991dc81278c707cba5be28df22
Steven Breziat <Takoros>
2023-07-07 17:58:04 +0200

    Renforce la sécurité dans le MainApiController

--- Symfony/src/Controller/API/MainAPIController.php
+++ Symfony/src/Controller/API/MainAPIController.php
@@ -9,22 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainAPIController extends AbstractAPIController
 {   
-    private $apiService;
-
-    public function __construct(APIService $apiService)
-    {
-        $this->apiService = $apiService;
-    }
-
     /**
      * Verify Token and reply 'pong' if good
      */
     #[Route('/api/ping', name: 'api_ping', methods:["POST"])]
-    public function ping(Request $request): JsonResponse
+    public function ping(): JsonResponse
     {
-        $post = json_decode($request->getContent());
-        $isValid = $this->verifyTokenAndData($post, [], $this->apiService);
-
         return new JsonResponse(['message' => 'pong !'], 200);
     }
 }
