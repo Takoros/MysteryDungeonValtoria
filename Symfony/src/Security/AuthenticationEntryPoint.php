448636662fc1ba02cef26d0404de6a80f8f7016c
Steven Breziat <Takoros>
2023-07-11 18:15:57 +0200

    Ajoute la nouvelle page d'accueil

--- Symfony/src/Security/AuthenticationEntryPoint.php
+++ Symfony/src/Security/AuthenticationEntryPoint.php
@@ -22,6 +22,6 @@ class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
 
         $session->getFlashBag()->add('danger', 'Vous devez être connecté pour accéder à cette page.');
 
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 }
